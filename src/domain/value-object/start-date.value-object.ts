d564c02481542c8cebecf79dc51e26df6824986e
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2022-08-01 20:49:49 -0300

    feat(value-object): created startDate

---
+++ src/domain/value-object/start-date.value-object.ts
@@ -0,0 +1,11 @@
+export class StartDate {
+  constructor(private _value: Date, dateComparison = new Date()) {
+    if (_value > dateComparison) {
+      throw new Error('startDate in the future');
+    }
+  }
+
+  public get value(): number {
+    return this._value.getTime();
+  }
+}
