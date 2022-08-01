e87a2fceea589ec710d3ce08f1d7695fae270022
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2022-08-01 20:34:26 -0300

    test(broker): ajusted test

--- src/domain/value-object/broker.value-object.spec.ts
+++ src/domain/value-object/broker.value-object.spec.ts
@@ -1,10 +1,14 @@
 import { Broker } from './broker.value-object';
 
 describe('Broker - Value Object ', () => {
-  const valueInvalid = [undefined, null, '', 'x'.repeat(151)];
+  const valueInvalid = [undefined, null, ''];
 
-  it.each(valueInvalid)('shold THROW value if parameter is invalid', (invalid) => {
+  it.each(valueInvalid)('shold THROW value if parameter is %s', (invalid) => {
     expect(() => new Broker(invalid)).toThrow();
+  });
+
+  it('shold THROW value if parameter more 150 character', () => {
+    expect(() => new Broker('x'.repeat(151))).toThrow();
   });
 
   it('shold CREATED value if parameter is valid', () => {
