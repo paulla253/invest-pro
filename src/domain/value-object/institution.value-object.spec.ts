0cd1e0526adce6bfaccb7ab52dc6423eb259e34e
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2022-08-01 20:30:47 -0300

    test: ajusted test

--- src/domain/value-object/institution.value-object.spec.ts
+++ src/domain/value-object/institution.value-object.spec.ts
@@ -1,10 +1,14 @@
 import { Institution } from './institution.value-object';
 
 describe('Institution - Value Object ', () => {
-  const valueInvalid = [undefined, null, '', 'x'.repeat(151)];
+  const valueInvalid = [undefined, null, ''];
 
-  it.each(valueInvalid)('shold THROW value if parameter is invalid', (invalid) => {
+  it.each(valueInvalid)('shold THROW value if parameter %s', (invalid) => {
     expect(() => new Institution(invalid)).toThrow();
+  });
+
+  it('shold THROW value if parameter more 150 character', () => {
+    expect(() => new Institution('x'.repeat(151))).toThrow();
   });
 
   it('shold CREATED value if parameter is valid', () => {
