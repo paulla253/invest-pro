d2f238ad77a2dd1e6f3d0acda84bafba8b098599
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2022-08-01 20:16:02 -0300

    feat(value-object): created institution

---
+++ src/domain/value-object/institution.value-object.ts
@@ -0,0 +1,14 @@
+export class Institution {
+  constructor(private _value: string) {
+    if (!_value) {
+      throw new Error('type is requeried');
+    }
+    if (_value.length > 150) {
+      throw new Error('type is length max 150');
+    }
+  }
+
+  public get value(): string {
+    return this._value.toUpperCase();
+  }
+}
