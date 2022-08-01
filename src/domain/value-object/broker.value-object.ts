575829edae60a8420bfd02f600ccf6a7428371f7
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2022-08-01 20:22:29 -0300

    refactor: description error

--- src/domain/value-object/broker.value-object.ts
+++ src/domain/value-object/broker.value-object.ts
@@ -2,10 +2,10 @@ export class Broker {
   private MAX_CHARACTER = 150;
   constructor(private _value: string) {
     if (!_value) {
-      throw new Error('type is requeried');
+      throw new Error('broker is requeried');
     }
     if (_value.length <= this.MAX_CHARACTER) {
-      throw new Error('type is length max ' + this.MAX_CHARACTER);
+      throw new Error('broker is length max ' + this.MAX_CHARACTER);
     }
   }
 
