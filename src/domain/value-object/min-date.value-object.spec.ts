c15335a1e2ec1e36af90644bc490c41f3b410acb
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2022-08-02 21:01:34 -0300

    test(value-object): created min-date

---
+++ src/domain/value-object/min-date.value-object.spec.ts
@@ -0,0 +1,17 @@
+import { EndDate as MinDate } from './end-date.value-object';
+
+describe('MinDate - Value Object ', () => {
+  const valueInvalid = [null, undefined];
+
+  it.each(valueInvalid)('shold THROW value if parameter is %s', (invalid) => {
+    expect(() => new MinDate(invalid)).toThrow();
+  });
+
+  it('shold CREATE value if parameter is valid', () => {
+    const date = new Date(2022, 8, 1);
+
+    const startDate = new MinDate(date);
+
+    expect(startDate.value).toBe(1662001200000);
+  });
+});
