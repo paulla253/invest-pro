d25d7e45439ba887eabc60ecef2457f0666710e4
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2022-08-02 21:36:32 -0300

    fix: added requeried

--- src/domain/value-object/start-date.value-object.ts
+++ src/domain/value-object/start-date.value-object.ts
@@ -1,8 +1,7 @@
 export class StartDate {
   constructor(private _value: Date, dateComparison = new Date()) {
-    if (_value > dateComparison) {
-      throw new Error('startDate in the future');
-    }
+    if (!_value) throw new Error('StartDate is requeried');
+    if (_value > dateComparison) throw new Error('startDate in the future');
   }
 
   public get value(): number {
