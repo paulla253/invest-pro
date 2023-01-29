c5d0a61fae144f1a622fc1c4d5d074262efa3bd8
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-01-29 20:06:53 -0300

    test: add test is number positive

--- src/domain/value-object/rate.value-object.spec.ts
+++ src/domain/value-object/rate.value-object.spec.ts
@@ -1,7 +1,7 @@
 import { Rate } from './rate.value-object';
 
 describe('Rate - Value object', () => {
-  const valueInvalid = [undefined, null, NaN, 0];
+  const valueInvalid = [undefined, null, NaN, 0, -5];
 
   it.each(valueInvalid)('shold THROW value if parameter %s', (invalid) => {
     expect(() => new Rate(invalid)).toThrow();
@@ -9,6 +9,6 @@ describe('Rate - Value object', () => {
 
   it('shold CREATED value if parameter is valid', () => {
     const investimentRate = new Rate(100);
-    expect(investimentRate.value).toBe(100);
+    expect(investimentRate.value()).toBe(100);
   });
 });
