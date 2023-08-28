14dab56c5941f06de818ec905da9400aa0f03f50
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-08-27 22:05:55 -0300

    chore: add config for module i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { AcceptLanguageResolver, I18nModule, QueryResolver, HeaderResolver } from 'nestjs-i18n';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import FixedIncomeModule from './infrastructure/module/fixed-income/fixed-income.module';
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: join(__dirname, '../i18n'),
+        watch: true,
+      },
+      resolvers: [{ use: QueryResolver, options: ['lang'] }, AcceptLanguageResolver, new HeaderResolver(['x-lang'])],
+    }),
+    FixedIncomeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
