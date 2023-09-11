71e759368dc490bfb388b17548bb1a26268d48de
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-09-11 20:29:34 -0300

    test: refactor description and fix test

--- src/domain/value-object/broker.value-object.spec.ts
+++ src/domain/value-object/broker.value-object.spec.ts
@@ -3,16 +3,16 @@ import { Broker } from './broker.value-object';
 describe('Broker - Value Object', () => {
   const valueInvalid = [undefined, null, ''];
 
-  it.each(valueInvalid)('shold THROW value if parameter is %s', (invalid) => {
+  it.each(valueInvalid)('should THROW value if parameter is %s', (invalid) => {
     expect(() => new Broker(invalid)).toThrow();
   });
 
-  it('shold THROW value if parameter more 150 character', () => {
+  it('should THROW value if parameter more 150 character', () => {
     expect(() => new Broker('x'.repeat(151))).toThrow();
   });
 
-  it('shold CREATED value if parameter is valid', () => {
+  it('should CREATED value if parameter is valid', () => {
     const type = new Broker('Broker Test');
-    expect(type.value()).toBe('BROKER TEST');
+    expect(type.value).toBe('BROKER TEST');
   });
 });
