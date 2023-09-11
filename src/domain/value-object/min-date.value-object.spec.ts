21cc6915ceff849462f25307ff89a529c293993a
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-09-11 20:31:12 -0300

    test: refactor description and fix test

--- src/domain/value-object/min-date.value-object.spec.ts
+++ src/domain/value-object/min-date.value-object.spec.ts
@@ -3,15 +3,15 @@ import { MinDate } from './min-date.value-object';
 describe('MinDate - Value Object', () => {
   const valueInvalid = [null, undefined];
 
-  it.each(valueInvalid)('shold THROW value if parameter is %s', (invalid) => {
+  it.each(valueInvalid)('should THROW value if parameter is %s', (invalid) => {
     expect(() => new MinDate(invalid)).toThrow();
   });
 
-  it('shold CREATE value if parameter is valid', () => {
+  it('should CREATE value if parameter is valid', () => {
     const date = new Date(2022, 8, 1);
 
     const minDate = new MinDate(date);
 
-    expect(minDate.value()).toBe(1662001200000);
+    expect(minDate.value).toBe('2022-09-01');
   });
 });
